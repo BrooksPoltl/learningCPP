102fd64803936d920aa7b6052af1fdf3d428c722
brookspoltl <bpoltl1@gmail.com>
2019-07-12 16:53:02 -0500

    favorite number

--- firstProgram/src/main.cpp
+++ firstProgram/src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main(){
-    std::cout <<"Hello world" <<std::endl;
+    int favorite_number;
+
+    std::cout <<"Enter your favorite number between 1 and 100: ";
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! thats my favorite number too" << std::endl;
     return 0;
 }
