5588f86b56c156032628a53d1f73e24602ae42bf
BrooksPoltl <bpoltl1@gmail.com>
2019-07-12 17:54:15 -0500

    setup workflow for c++

--- firstProgram/src/main.cpp
+++ firstProgram/src/main.cpp
@@ -2,9 +2,8 @@
 
 int main(){
     int favorite_number;
-
     std::cout <<"Enter your favorite number between 1 and 100: ";
     std::cin >> favorite_number;
-    std::cout << "Amazing!! thats my favorite number too" << std::endl;
+    std::cout << "Amazing!ts my favorite number too" << std::endl;
     return 0;
 }
