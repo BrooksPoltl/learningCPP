7e20855c09cb7f2d45e002e6cb9aac5a96df6411
BrooksPoltl <bpoltl1@gmail.com>
2019-07-15 08:45:15 -0500

    finished i/o

--- basicIO/src/main.cpp
+++ basicIO/src/main.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main (){
-    cout <<"Hello world!" << endl;
+    // cout << "Hello world!" << endl;
+    // cout << "Hello" << " world!" << endl;
+    // cout << "Hello"<< " world!\n";
+    // cout << "Hello\nOut\nThere\n";
+    int num1;
+    int num2;
+    double num3;
+    // cout << "Enter and integer: ";
+    // cin >> num1;
+    // cout << "You entered: "<< num1 << endl;
     return 0;
 }
