adf40ed31996e5d29c438db6a26dea6d5ee523bf
BrooksPoltl <bpoltl1@gmail.com>
2019-07-16 22:52:46 -0500

    comment out basicIO

--- basicIO/src/main.cpp
+++ basicIO/src/main.cpp
@@ -7,9 +7,9 @@ int main (){
     // cout << "Hello" << " world!" << endl;
     // cout << "Hello"<< " world!\n";
     // cout << "Hello\nOut\nThere\n";
-    int num1;
-    int num2;
-    double num3;
+    // int num1;
+    // int num2;
+    // double num3;
     // cout << "Enter and integer: ";
     // cin >> num1;
     // cout << "You entered: "<< num1 << endl;
