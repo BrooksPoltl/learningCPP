ff592705c6bd25bb84371a5e9df5641285dc3e08
BrooksPoltl <bpoltl1@gmail.com>
2019-07-17 19:57:22 -0500

    accessing and setting temps

--- arrays/src/main.cpp
+++ arrays/src/main.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(){
-    char vowels [] {'a','e','i','o','u'};
-    cout << "\n The first vowel is: " << vowels[0]<<endl;
-    cout << "The last vowel is: "<< vowels[4]<<endl;
+    double hi_temps [] {90.1,89.8,77.5,81.6};
+    cout <<"\n The first high temperature is: " << hi_temps[0]<<endl;
+    hi_temps[0] = 100.7;
+    cout << "The first high temperature is now: "<< hi_temps[0]<<endl;
     return 0;
 }
