0ffd997b92c97ea5572823df248d5b719040681f
BrooksPoltl <bpoltl1@gmail.com>
2019-07-17 21:55:01 -0500

    vector challenge setup

--- arrays/src/main.cpp
+++ arrays/src/main.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 int main(){
-    int test_scores[5] {};
+    int test_scores[] {100,90,80,70,60};
     cout << "\n first score at index 0:"<< test_scores[0]<<endl;
     cout << "second score at index 1:"<< test_scores[1]<<endl;
     cout << "third score at index 2:"<< test_scores[2]<<endl;
     cout << "fourth score at index 3:"<< test_scores[3]<<endl;
     cout << "fifth score at index 4:"<< test_scores[4]<<endl;
 
+    cout << "value of the array: "<< test_scores << endl;
     return 0;
 }
---
+++ vectorChallenge/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iosteam>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector <int> vector1;
+    vector <int> vector2;
+    
+    return 0;
+}
