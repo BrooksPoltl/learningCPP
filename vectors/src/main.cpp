646b01ccbf8df0d43075764ba5142c55fc122354
BrooksPoltl <bpoltl1@gmail.com>
2019-07-17 21:38:34 -0500

    at method

--- vectors/src/main.cpp
+++ vectors/src/main.cpp
@@ -8,10 +8,10 @@ int main(){
     vector <int> test_scores {100,98,89,85,93};
     // vector <double> hi_temperatures (365,80.0);
 
-    cout << "\n first score at index 0:"<< test_scores[0]<<endl;
-    cout << "second score at index 1:"<< test_scores[1]<<endl;
-    cout << "third score at index 2:"<< test_scores[2]<<endl;
-    cout << "fourth score at index 3:"<< test_scores[3]<<endl;
-    cout << "fifth score at index 4:"<< test_scores[4]<<endl;
+    cout << "\n first score at index 0:"<< test_scores.at(0)<<endl;
+    cout << "second score at index 1:"<< test_scores.at(1)<<endl;
+    cout << "third score at index 2:"<< test_scores.at(2)<<endl;
+    cout << "fourth score at index 3:"<< test_scores.at(3)<<endl;
+    cout << "fifth score at index 4:"<< test_scores.at(4)<<endl;
     return 0;
 }
