fe99d0a973f756a4227f0ac31f0bb04507440195
BrooksPoltl <bpoltl1@gmail.com>
2019-07-18 09:04:14 -0500

    arithmetic

--- operators/src/main.cpp
+++ operators/src/main.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 int main(){
-    int num1 {10};
-    int num2 {20};
+    int num1 {200};
+    int num2 {100};
 
-    num1 = num2 = 1000;
 
-    cout << "num1 is "<< num1 << endl;
-    cout << "num2 is "<< num2 << endl;
-
+    cout << num1 << " + "<< num2 << " = "<< num1+num2<< endl;
+    cout << num1 << " - "<< num2 << " = "<< num1-num2<< endl;
+    cout << num1 << " * "<< num2 << " = "<< num1*num2<< endl;
+    cout << num1 << " / "<< num2 << " = "<< num1/num2<< endl;
+    cout << num1 << " % "<< num2 << " = "<< num1%num2<< endl;
     cout << endl;
     return 0;
 }
