dcdd91b1f44e7e07d4ca0087352ddf5615d23104
BrooksPoltl <bpoltl1@gmail.com>
2019-07-17 21:37:09 -0500

    accessing vectors

--- vectors/src/main.cpp
+++ vectors/src/main.cpp
@@ -4,8 +4,14 @@
 using namespace std;
 
 int main(){
-    vector <char> vowels {'a','e','i','o',u};
+    // vector <char> vowels {'a','e','i','o',u};
     vector <int> test_scores {100,98,89,85,93};
-    vector <double> hi_temperatures (365,80.0);
+    // vector <double> hi_temperatures (365,80.0);
+
+    cout << "\n first score at index 0:"<< test_scores[0]<<endl;
+    cout << "second score at index 1:"<< test_scores[1]<<endl;
+    cout << "third score at index 2:"<< test_scores[2]<<endl;
+    cout << "fourth score at index 3:"<< test_scores[3]<<endl;
+    cout << "fifth score at index 4:"<< test_scores[4]<<endl;
     return 0;
 }
