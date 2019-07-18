921d2b4d7baae371cb49eaa47222fb1983b6f95b
BrooksPoltl <bpoltl1@gmail.com>
2019-07-17 20:02:04 -0500

    uninitialized array

--- arrays/src/main.cpp
+++ arrays/src/main.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 
 int main(){
-    double hi_temps [] {90.1,89.8,77.5,81.6};
-    cout <<"\n The first high temperature is: " << hi_temps[0]<<endl;
-    hi_temps[0] = 100.7;
-    cout << "The first high temperature is now: "<< hi_temps[0]<<endl;
+    int test_scores[5];
+    cout << "\n first score at index 0:"<< test_scores[0]<<endl;
+    cout << "second score at index 1:"<< test_scores[1]<<endl;
+    cout << "third score at index 2:"<< test_scores[2]<<endl;
+    cout << "fourth score at index 3:"<< test_scores[3]<<endl;
+    cout << "fifth score at index 4:"<< test_scores[4]<<endl;
+
     return 0;
 }
