1d452ef59fa27da01f163e0e4687d6013e4a17cf
BrooksPoltl <bpoltl1@gmail.com>
2019-07-18 09:27:30 -0500

    using pre-increment

--- incrementDecrement/src/main.cpp
+++ incrementDecrement/src/main.cpp
@@ -6,16 +6,22 @@ int main(){
     int counter {10};
     int result {0};
 
+    // cout << "Counter: "<< counter <<endl;
+
+    // counter = counter +1;
+    // cout << "Counter: "<< counter <<endl;
+
+    // counter++;
+    // cout << "Counter: "<< counter <<endl;
+
+    // ++counter;
+    // cout << "Counter: "<< counter <<endl;
+
     cout << "Counter: "<< counter <<endl;
 
-    counter = counter +1;
+    result = ++counter;
     cout << "Counter: "<< counter <<endl;
-
-    counter++;
-    cout << "Counter: "<< counter <<endl;
-
-    ++counter;
-    cout << "Counter: "<< counter <<endl;
+    cout << "Result: "<< result << endl;
 
     return 0;
 }
