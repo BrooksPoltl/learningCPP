5205ecb0bdffed19c3761be471c86e490a080703
BrooksPoltl <bpoltl1@gmail.com>
2019-07-17 20:03:18 -0500

    initializing 0

--- arrays/src/main.cpp
+++ arrays/src/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    int test_scores[5];
+    int test_scores[5] {};
     cout << "\n first score at index 0:"<< test_scores[0]<<endl;
     cout << "second score at index 1:"<< test_scores[1]<<endl;
     cout << "third score at index 2:"<< test_scores[2]<<endl;
